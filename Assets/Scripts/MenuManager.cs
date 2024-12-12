d6a3b823bf7107c39ece74494f63c980b2aded4a
Andre Sousa <up202005277@up.pt>
2024-12-12 17:39:39 +0000

    did start menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using Unity.Loading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+    public GameObject loading;
+    public GameObject toDisappearDuringInstructions;
+
+    public void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+        toDisappearDuringInstructions.SetActive(false);
+    }
+
+    public void HideInstructions()
+    {
+        Debug.Log("hello!");
+        instructionsPanel.SetActive(false);
+        toDisappearDuringInstructions.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        loading.SetActive(true);
+        StartCoroutine(LoadSceneAsync("SampleScene"));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the scene is fully loaded
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
