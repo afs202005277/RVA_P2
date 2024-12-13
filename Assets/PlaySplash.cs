b5bacaf86f842c909350f0aa542a6b184cb8b6fa
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2024-12-13 20:23:38 +0000

    Water splash sound on items

---
+++ Assets/PlaySplash.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySplash : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            _audioSource.Play();
+        }
+    }
+}
