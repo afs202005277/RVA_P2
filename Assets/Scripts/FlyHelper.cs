5241ed29cd7a443800fb076ebf396f3e901147a6
Marcelo <marceloguarniero@gmail.com>
2024-12-14 16:23:14 +0000

    Added the ability to freeze height while flying

--- Assets/Scripts/FlyHelper.cs
+++ Assets/Scripts/FlyHelper.cs
@@ -2,17 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class FlyHelper : MonoBehaviour
 {
+    public InputActionReference freezeHeightAction;
+    public DynamicMoveProvider dynamicMoveProvider;
     public float gravity = 0.1f;
     public float offset = 0.2f;
     public Terrain terrain;
     private float _lastY = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        freezeHeightAction.action.started += ctx => ToggleFreezeHeight(true);
+        freezeHeightAction.action.canceled += ctx => ToggleFreezeHeight(false);
+    }
+
+    void ToggleFreezeHeight(bool isFrozen)
+    {
+        dynamicMoveProvider.enableFly = !isFrozen;
+        dynamicMoveProvider.useGravity = !isFrozen;
     }
 
     private void FixedUpdate()
