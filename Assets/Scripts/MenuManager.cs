1bff89bcbb2e036512ae14b3b60c7bed9293ac32
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2024-12-14 16:43:42 +0000

    Menu fix

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -19,7 +19,6 @@ public class MenuManager : MonoBehaviour
 
     public void HideInstructions()
     {
-        Debug.Log("Hide!");
         instructionsPanel.SetActive(false);
         toDisappearDuringInstructions.SetActive(true);
     }
